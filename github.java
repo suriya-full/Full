99fd992d70dc7c59de32e75de05b9677702ab62e
suriya-full <suriya.kamal@anywhere.co>
2019-12-10 12:30:15 +0530

    First Commit

---
+++ github.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class github {
+
+	//Main Method -1
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello git hub");
+
+	}
+
+}
