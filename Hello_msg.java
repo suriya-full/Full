212e0fdda600724965ebbc40a7e129b6ecb01d47
suriya-full <suriya.kamal@anywhere.co>
2019-12-10 11:39:03 +0530

    gsg

---
+++ Hello_msg.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class Hello_msg {
+	public static void main(String args[])
+	{
+	System.out.println("Hello people");
+	}
+}
---
+++ github.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello git hub");
+
+	}
+
+}
